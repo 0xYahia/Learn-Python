4aa9acf6a48c7dcdc0cbeeb438f15d4b9e96214e
yahia <mohamedyahia207545>
2023-04-26 01:10:19 +0200

    external module

---
+++ Using external code/Python built-in modules/03_03.py
@@ -0,0 +1,12 @@
+import math;
+print(math.sqrt(4))
+print(math.pow(2,3))
+
+import random;
+print(random.randint(1,10))
+
+import keyword;
+print(keyword.iskeyword('if'))
+
+import uuid
+print(uuid.uuid4())
