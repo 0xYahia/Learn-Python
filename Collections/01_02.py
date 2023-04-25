636ad2f9fd30397601774cd8b67f6244d008320c
yahia <mohamedyahia207545>
2023-04-26 00:00:14 +0200

    first commit

---
+++ Collections/01_02.py
@@ -0,0 +1,13 @@
+yahia='01027207545'
+yousef='01027207546'
+bishoy='01027207547'
+mohamed='01027207548'
+
+phoneBook = {
+    'yahia':'01027207545',
+    'yousef':'01027207546',
+    'bishoy':'01027207547',
+    'mohamed':'01027207548',
+}
+
+print(phoneBook['bishoy'])
---
+++ Collections/01_05.py
@@ -0,0 +1,32 @@
+# Neatest stars to Earth
+star1 = "Sol"
+star2 = "Alpha Centauri"
+star3 = "Baranard"
+star4 = "Wolf 359"
+
+# Highest peak on each teactonic plate
+African = "Kilimanjaro"
+Antarctic = "Vinson"
+Eurasian = "Everest"
+North_American = "Denali"
+Pacific = "Mauna Kea"
+
+stars = [
+    star1,
+    star2,
+    star3,
+    star4,
+]
+
+print(stars[3])
+
+peaks = {
+    African: "Kilimanjaro",
+    Antarctic: "Vinson",
+    Eurasian: "Everest",
+    North_American: "Denali",
+    Pacific: "Mauna Kea",
+    Pacific: "Mauna Kea",
+}
+
+print(peaks[Pacific])
