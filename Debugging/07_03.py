aa166181286f4948586af7091558dcadc0d2091b
yahia <mohamedyahia207545>
2023-04-27 15:04:21 +0200

    debugging

---
+++ Debugging/07_03.py
@@ -0,0 +1,7 @@
+temperature = 50
+
+# if temp < 60
+#   print(Bring a jacket)
+
+if temperature < 60:
+    print("Bring a jacket")
---
+++ Debugging/07_07.py
@@ -0,0 +1,15 @@
+def plant_recommendation(care):
+    if care == "low":
+        print("Sabar")
+    elif care == "medium":
+        print("Liblab")
+    elif care == "high":
+        print("Orcade")
+
+
+plant_recommendation("low")
+plant_recommendation("medium")
+plant_recommendation("high")
+
+if "1" != 1:
+    print("hello")
