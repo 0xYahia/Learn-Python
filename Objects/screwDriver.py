e24f621ab5a7abd75117bb87b5701abbb6b098f9
yahia <mohamedyahia207545>
2023-04-28 03:24:21 +0300

    tsst_cases

--- Objects/screwDriver.py
+++ Objects/screwDriver.py
@@ -19,3 +19,28 @@ class Screwdriver:
             print(result + "and I test electricity")
         else:
             print(result + "and I don't test electricity")
+
+
+list1 = ["ali", "ahmed", "mohamed"]
+list1.append("alaa")
+print(list1)
+
+name = "Ahmed Fathy"
+print(name[:5])
+
+name = "Ahmed Fathy"
+print(name[:])
+
+name = "Ahmed Fathy"
+print(name[6:])
+
+name = "Ali" * 2 * 3
+print(name)
+
+# num = [10, 20, 30, 40, 50]
+# num.remove(60)
+# print(num)
+
+num = [10, 20, 30, 40, 50]
+num.remove(30)
+print(num)
---
+++ teast_cases.py
@@ -0,0 +1,23 @@
+list1 = ["ali", "ahmed", "mohamed"]
+list1.append("alaa")
+print(list1)
+
+name = "Ahmed Fathy"
+print(name[:5])
+
+name = "Ahmed Fathy"
+print(name[:])
+
+name = "Ahmed Fathy"
+print(name[6:])
+
+name = "Ali" * 2 * 3
+print(name)
+
+# num = [10, 20, 30, 40, 50]
+# num.remove(60)
+# print(num)
+
+num = [10, 20, 30, 40, 50]
+num.remove(30)
+print(num)
